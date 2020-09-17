2be5a98c5d3024021faad5a18340d834c607383e
Nicole Dow <nikki.dow71@gmail.com>
2020-09-16 18:27:55 -0700

    add title and image to book swiper

--- src/Components/BookSwiper.js
+++ src/Components/BookSwiper.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Typography } from "@material-ui/core";
 
-const BookSwiper = props => {
-    console.log(props);
-    return(
-        <div>
-            {props.book.title}
-        </div>
-    )
+const BookSwiper = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <img
+        src={props.book.book_image}
+        height={props.book.book_image_height}
+        width={props.book.book_image_width}
+      />
+      <Typography variant="h2">{props.book.title}</Typography>
+      {props.book.description}
+    </div>
+  );
 };
 
 export default BookSwiper;
