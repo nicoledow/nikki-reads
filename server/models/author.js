00b98fd817853006b91fdde038d628f1125799b7
Nicole Dow <nikki.dow71@gmail.com>
2020-09-25 16:50:40 -0700

    create author schema

---
+++ server/models/author.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const authorSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    books: [{ type: Schema.Types.ObjectId, ref: 'Book'}]
+});
+
+module.exports = mongoose.model('Author', authorSchema);
