50202b696216d4ac2b3fe25d4e3b58d71a29ceb6
Nicole Dow <nikki.dow71@gmail.com>
2020-09-26 13:18:14 -0700

    change grid columns based on screen size

--- client/src/Components/BookListGrid.js
+++ client/src/Components/BookListGrid.js
@@ -1,28 +1,54 @@
 import React from "react";
-import { GridList, GridListTile, Typography } from "@material-ui/core";
+import {
+  GridList,
+  GridListTile,
+  Typography,
+  makeStyles,
+  withWidth,
+  isWidthUp,
+} from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-const BookListGrid = (props) => {
-  console.log(props)
-  const showList = (event) => {
-    console.log(props.lists);
-    const listTitle = event.target.id;
-    console.log("list title", listTitle);
+function BookListGrid(props) {
+  const useStyles = makeStyles({
+    cardStyle: {
+      border: "1px solid #20c4cc",
+      width: window.width / calculateCols()
+    },
+  });
+  const classes = useStyles();
+
+  function calculateCols() {
+    const width = props.width;
+    
+    if (isWidthUp("xl", width) || isWidthUp("lg", width)) {
+      return 5;
+    } else if (isWidthUp("md", width)) {
+      return 3;
+    } else if (isWidthUp('sm', width)) {
+      return 2;
+    } else {
+      return 1;
+    }
   };
 
+  const cols = calculateCols();
+  console.log('cols', cols);
   return (
-    <GridList cols={5}>
+    <GridList cols={cols}>
       {props.lists.map((list) => {
         return (
-          <GridListTile>
+          <GridListTile className={classes.cardStyle}>
             <button>
-              <Link to={`/lists/${list.list_name_encoded}`}>{list.display_name}</Link>
+              <Link to={`/lists/${list.list_name_encoded}`}>
+                {list.display_name}
+              </Link>
             </button>
           </GridListTile>
         );
       })}
     </GridList>
   );
-};
+}
 
-export default BookListGrid;
+export default withWidth()(BookListGrid);
--- client/src/Components/ExploreOptionsMenu.js
+++ client/src/Components/ExploreOptionsMenu.js
@@ -1,45 +1,41 @@
-import React, { useState } from 'react';
-import BookListGrid from './BookListGrid';
-    
-const dotenv = require('dotenv');
+import React, { useState } from "react";
+import BookListGrid from "./BookListGrid";
+
+const dotenv = require("dotenv");
 const nytBaseUrl = process.env.REACT_APP_NYT_BOOKS_API_BASE;
 
 const ExploreOptionsMenu = () => {
-    const [lists, setLists] = useState([]);
-    const [listsShouldDisplay, showOrHideLists] = useState(false);
+  const [lists, setLists] = useState([]);
+  const [listsShouldDisplay, showOrHideLists] = useState(false);
 
+  const fetchNYTLists = () => {
+    const url = `${nytBaseUrl}/lists/names.json?api-key=${process.env.REACT_APP_NYT_API_KEY}`;
+    fetch(url)
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        const lists = data.results;
+        setLists(lists);
+        showOrHideLists(true);
+      })
+      .catch((err) => console.log("err", err));
+  };
 
-    const fetchNYTLists = () => {
-        const url = `${nytBaseUrl}/lists/names.json?api-key=${process.env.REACT_APP_NYT_API_KEY}`;
-        fetch(url)
-        .then(resp => {
-            return resp.json()
-        })
-        .then(data => {
-            const lists = data.results;
-            setLists(lists)
-            showOrHideLists(true);
-        })
-        .catch(err => console.log('err', err));
-    }
+  const openNYTLists = () => {
+    fetchNYTLists();
+  };
 
-    const openNYTLists = () => {
-        fetchNYTLists();
-    };
-
-    return(
-        <div>
-            Explore options menu:
-            <br/>
-            <button onClick={openNYTLists}>New York Times Lists</button>
-            {
-                listsShouldDisplay && lists.length > 0 ?
-                <BookListGrid lists={lists}/>
-                :
-                <div/>
-            }
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={openNYTLists}>New York Times Lists</button>
+      {listsShouldDisplay && lists.length > 0 ? (
+        <BookListGrid lists={lists} />
+      ) : (
+        <div />
+      )}
+    </div>
+  );
 };
 
 export default ExploreOptionsMenu;
