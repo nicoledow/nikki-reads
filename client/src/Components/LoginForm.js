31532f3957a76c5b5e24211618a0173d65a9c227
Nicole Dow <nikki.dow71@gmail.com>
2020-10-25 17:32:46 -0400

    sign up form and validations

--- client/src/Components/LoginForm.js
+++ client/src/Components/LoginForm.js
@@ -1,19 +1,49 @@
 import React from 'react';
+import { useState } from "react";
 import { Container, Typography } from '@material-ui/core';
 
 function LoginForm(props) {
-  console.log(props);
-  const handleSignup = () => {
-    // e.preventDefault();
-    console.log('handle signup fn');
+
+  const handleSignup = e => {
+    e.preventDefault();
+
+    const inputs = [...e.target.querySelectorAll('input')];
+
+    const formData = {}
+    inputs.forEach(input => {
+      if (input.type !== 'submit') {
+        formData[input.name] = input.value;
+      }
+    });
+    
+    if (validateInputs(formData)) {
+      console.log('can be submitted');
+    } else {
+      console.log('failed validations');
+    }
+  };
+
+  const validateInputs = data => {
+    if (data.password !== data.confirmPassword) {
+      alert('Passwords do not match!');
+      return false;
+    } else if (!Object.values(data).every(value => value !== '')) {
+      alert('All fields are required!');
+      return false;
+    } else {
+      return true;
+    }
   }
-  
-  if(props.form === 'signup') {
+
+  if (props.form === 'signup') {
     return (
       <Container>
         <form onSubmit={handleSignup}>
-          <input type="text" name="email" placeholder="Email address"/>
-          <input type="submit" value="Sign Up"/>
+          <input type="text" name="name" placeholder="Name" /><br />
+          <input type="text" name="email" placeholder="Email address" /><br />
+          <input type="password" name="password" placeholder="Password" /><br />
+          <input type="password" name="confirmPassword" placeholder="Confirm Password" /><br />
+          <input type="submit" value="Sign Up" />
         </form>
       </Container>
     )
