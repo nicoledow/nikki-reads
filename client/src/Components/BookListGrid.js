26264ef19be47aee2e7b1ed6fe45f6ab672c067c
Nicole Dow <nikki.dow71@gmail.com>
2020-11-15 10:31:06 -0700

    changing class name on list select is now working

--- client/src/Components/BookListGrid.js
+++ client/src/Components/BookListGrid.js
@@ -1,15 +1,23 @@
-import React from "react";
-import { Grid, Container } from "@material-ui/core";
-import ListTile from '../Components/ListTile';
+import React, { useState } from "react";
+import { Grid, Container, makeStyles } from "@material-ui/core";
+import ListTile from "../Components/ListTile";
+import Theme from "../Theme/Theme.js";
 
 function BookListGrid(props) {
+  const [selectedList, selectList] = useState(null);
+  console.log("booklistgrid state - selectedList", selectedList);
 
   return (
     <Container>
       <Grid container spacing={5}>
-        {props.lists.map((list) => {
+        {props.lists.slice(0, 3).map((list) => {
           return (
-            <ListTile list={list}/>
+            <ListTile
+              key={list.list_name_encoded}
+              list={list}
+              selected={selectedList === list.display_name ? true : false}
+              selectList={selectList}
+            />
           );
         })}
       </Grid>
--- client/src/Components/ListTile.js
+++ client/src/Components/ListTile.js
@@ -3,14 +3,15 @@ import { Grid, makeStyles } from "@material-ui/core";
 import Theme from "../Theme/Theme";
 
 export default function ListTile(props) {
-  const listName = props.list.list_name_encoded;
-
-  console.log("list", props.list);
+  const listName = props.list.display_name;
+  const isSelected = props.selected;
 
   const handleClick = e => {
-    e.target.parentElement.querySelector('div').className = classes.selectedCardHead;
+    props.selectList(prevList => {
+      return listName;
+    })
   };
-
+  
   const useStyles = makeStyles({
     cardStyle: {
       ...Theme.card,
@@ -35,11 +36,10 @@ export default function ListTile(props) {
   const classes = useStyles();
 
   return (
-    <Grid item xs={12} sm={6} md={3} className={classes.cardStyle} onClick={e => handleClick(e)}>
-      {/* <div style={{height: "2rem", backgroundColor: Theme.palette.highlight}}/> */}
-      <div className={classes.unselectedCardHead}/>
-      <h3 className={classes.linkStyle}>
-        {props.list.list_name}
+    <Grid item xs={12} sm={6} md={3} className={classes.cardStyle}>
+      <div className={isSelected ? classes.selectedCardHead : classes.unselectedCardHead}/>
+      <h3 className={classes.linkStyle} onClick={e => handleClick(e)}>
+        {listName}
       </h3>
     </Grid>
   );
