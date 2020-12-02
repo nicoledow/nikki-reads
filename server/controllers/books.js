6e335b7089c84daa68f9b6b1acb3e873f76484c3
Nicole Dow <nikki.dow71@gmail.com>
2020-12-01 19:58:20 -0700

    fix models

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -1,37 +1,52 @@
-const _ = require('lodash/string');
+const _ = require("lodash/string");
 
-const Book = require('../models/book');
-const User = require('../models/user');
+const Book = require("../models/book");
+const User = require("../models/user");
 
 exports.likeBook = (req, res, next) => {
+  const user = User.findById(req.body.user.id);
+  let book = Book.findOne({ isbn10: req.body.isbn10 });
 
-    const book = new Book({
-        title: _.capitalize(req.body.title),
-        buyURL: req.body.buyURL,
-        // author: author,
-        imageUrl: req.body.imageUrl,
-        nytBookURI: req.body.bookURI,
-        isbn10: req.body.isbn10
+  if (!book) {
+    book = new Book({
+      title: _.capitalize(req.body.title),
+      buyURL: req.body.buyURL,
+      // author: author,
+      imageUrl: req.body.imageUrl,
+      nytBookURI: req.body.bookURI,
+      isbn10: req.body.isbn10,
+      user: user._id,
     });
-    
-    book.save()
-    .then(result => {
+
+    book
+      .save()
+      .then((result) => {
         if (!result) {
-            const error = new Error('Book could not be saved.');
-            error.statusCode(500);
-            throw error;
+          const error = new Error("Book could not be saved.");
+          error.statusCode(500);
+          throw error;
         }
         const response = {
-            message: 'Book saved.',
-            book: { id: book._id.toString(), title: book.title, buyURL: book.buyURL, nytBookURI: book.nytBookURI, imageUrl: book.imageUrl }
-        }
+          message: "Book saved.",
+          book: {
+            id: book._id.toString(),
+            title: book.title,
+            buyURL: book.buyURL,
+            nytBookURI: book.nytBookURI,
+            imageUrl: book.imageUrl,
+          },
+        };
         console.log(response);
-        res.status(201).json(response)
-    })
+        res.status(201).json(response);
+      })
+      .catch((err) => console.log("error in saving book", err));
+  } else {
+      console.log('book exists');
+      //add this user's id to this book's array of userids
+  }
 };
 
 exports.getCurrentUserBooks = (req, res, next) => {
-    const userId = req.params.userId;
-    const user = User.findById(userId);
-    
-}
+  const userId = req.params.userId;
+  const user = User.findById(userId);
+};
--- server/models/book.js
+++ server/models/book.js
@@ -1,43 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String
-    },
-    isbn10: {
-        type: String,
-        required: true
-    },
-    // author: {
-    //     type: Schema.Types.ObjectId,
-    //     required: true,
-    //     ref: 'Author'
-    // },
-    read: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    rating: {
-        type: Number
-    },
-    buyURL: {
-        type: String
-    },
-    imageUrl: {
-        type: String,
-        required: true
-    },
-    nytBookURI: {
-        type: String,
-        required: true
-    },
-    tags: [{type: String}]
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+  },
+  isbn10: {
+    type: String,
+    required: true,
+  },
+  // author: {
+  //     type: Schema.Types.ObjectId,
+  //     required: true,
+  //     ref: 'Author'
+  // },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+  },
+  buyURL: {
+    type: String,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  nytBookURI: {
+    type: String,
+    required: true,
+  },
+  tags: [{ type: String }],
+  userIds: [{ type: String }]
 });
 
-module.exports = mongoose.model('Book', bookSchema);
+module.exports = mongoose.model("Book", bookSchema);
